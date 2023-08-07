bac286532d76c4369511f77de10c7ab2f624e5e8
= <steven.serruya@gmail.com>
2023-08-07 18:36:20 -0500

    asdded the generaterandomString

--- express_server.js
+++ express_server.js
@@ -8,7 +8,12 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(express.urlencoded({ extended: true }));
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -29,7 +34,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase.id };
   res.render("urls_show", templateVars);
 });
+
+
+
+
+
+const generateRandomString = () => {
+  const alphanumericChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+    result += alphanumericChars[randomIndex];
+  }
+  return result;
+};
